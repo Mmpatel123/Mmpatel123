92aa6be0b6eb1fd156e62c1cc22e2030efb10f87
Mmpatel123 <sweetmegs2604@gmail.com>
2021-12-16 21:30:11 +0000

    HOMEWORK

--- src/test/java/com/seleniumPractice/NowLoginNopCommernce.java
+++ src/test/java/com/seleniumPractice/NowLoginNopCommernce.java
@@ -1,2 +1,43 @@
-package com.seleniumPractice;public class NowLoginNopCommernce {
-}
+package com.seleniumPractice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class NowLoginNopCommernce {
+    WebDriver driver;
+    String LINK_URL="https://demo.nopcommerce.com/";
+    @Before
+    public void setUp(){
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+    }
+    @Test
+    public void openhome() throws InterruptedException {
+        driver.get(LINK_URL);
+
+      driver.findElement(By.xpath("/html/body/div[6]/div[1]/div[1]/div[2]/div[1]/ul/li[2]/a")).click();
+      Thread.sleep(500L);
+driver.findElement(By.id("Email")).sendKeys("lightyear9461@gmail.com");
+Thread.sleep(500L);
+driver.findElement(By.id("Password")).sendKeys("M630ela1");
+Thread.sleep(500L);
+driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/div/div/div[2]/div[1]/div[2]/form/div[3]/button")).click();
+
+        System.out.println(driver.getTitle());
+        Thread.sleep(1500L);
+
+    }
+    @After
+    public void tearDown(){
+driver.close();
+}}
